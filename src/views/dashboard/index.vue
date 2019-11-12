dd7737565c2bc027d175a49805dbe451d7c455c6
leijmdas@163.com <leijmdas@163.com>
2019-11-12 16:32:08 +0800

    add

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+
+    <el-carousel height="100%" width='100%' arrow="always" indicator-position="none"  >
+
+          <div class="mask" v-show="true">
+            <img name='img' src="../../assets/images/wallpaper.png" style="
+                              width: 100%;
+                              height: auto;"
+                 alt=""  >
+          </div>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
